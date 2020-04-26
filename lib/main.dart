afeb5f968698a09ffaa83c26535ced07e7892b77
Thiago <tfernandes.cunha@gmail.com>
2020-04-26 17:49:24 -0300

    Calculadora básica de IMC - Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -27,25 +27,35 @@ class _HomeState extends State<Home> {
   void calculate() {
     double weight = double.parse(weightController.text);
     double height = double.parse(heightController.text) / 100;
+    if (weight <= 0 || height <= 0) {
+      setState(() {
+        _infoText = 'Ops...preencha corretamente o formulário.';
+      });
+    }else {
+      double imc = weight / (height * height);
 
-    double imc = weight / (height * height);
-
-    debugPrint(imc.toString());
-    setState(() {
-      if (imc < 18.6) {
-        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 29.9 && imc < 34.9) {
-        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 34.9 && imc < 39.9) {
-        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 40) {
-        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
-      }
-    });
+      debugPrint(imc.toString());
+      setState(() {
+        if (imc < 18.6) {
+          _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+        } else
+        if (imc >= 18.6 && imc < 24.9) {
+          _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+        } else
+        if (imc >= 24.9 && imc < 29.9) {
+          _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+        } else
+        if (imc >= 29.9 && imc < 34.9) {
+          _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+        } else
+        if (imc >= 34.9 && imc < 39.9) {
+          _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+        } else
+        if (imc >= 40) {
+          _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+        }
+      });
+    }
   }
 
   @override
